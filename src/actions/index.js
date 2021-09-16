c0c6e73aa1407f7cca77afe95d0a16069893340e
Mr-BK <bogharakuldip@gmail.com>
2021-09-16 11:51:44 -0400

    First Commit

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+/* The reason why we choose to give name "index.js" for this file is 
+because while importing in other file we will only have to wright 
+directory name (../action) and it will automatically take index.js (root) 
+file. */
+
+//Action creator
+//this us  named export which allow us to export many different functions (action creators) from single file
+export const selectSong = () => {
+  //Return an action
+  return {
+    //type is mendatory but payload is not necessary
+    type: 'SONG_SELECTED',
+    payload: song
+  };
+};
---
+++ src/reducers/index.js
@@ -0,0 +1,25 @@
+//importing redux library (it example of named export)
+import { combineReducers } from 'redux';
+
+//Reducers
+const songsReducer = () => {
+  return [
+    { title: 'No scrubs', duration: '4:05' },
+    { title: 'Macarena', duration: '2:30' },
+    { title: 'All Star', duration: '3:15' },
+    { title: 'I want it that way', duration: '1:45' }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
